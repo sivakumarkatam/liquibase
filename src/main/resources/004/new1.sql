971ed85cb130a88ea9f09418ac977e47c64c6104
sivakumarkatam <sivascm91@gmail.com>
2019-01-30 14:50:01 +0530

    removed scripts

--- src/main/resources/004/new1.sql
+++
@@ -1,6 +0,0 @@
-create table test11 (
-    id int primary key,
-    name varchar(255)
-);
-
-insert into test11 (id, name) values (1, 'namenew' );
--- src/main/resources/004/withinsert.sql
+++
@@ -1,7 +0,0 @@
-create table test5 (
-    id int primary key,
-    name varchar(255)
-);
--- insert into test5 (id, name) values (1, ‘name 1′);
--- insert into test5 (id, name) values (2, ‘name 2′);
-
